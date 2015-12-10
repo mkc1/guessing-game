1e4a5a72e95b29fab393ef8b3137205536aea011
mkc1 <mkc1@users.noreply.github.com>
2015-12-09 19:04:37 -0500

    Create guessingGame.js

---
+++ guessingGame.js
@@ -0,0 +1,48 @@
+/* **** Global Variables **** */
+// try to elminate these global variables in your project, these are here just to start.
+
+var playersGuess,
+    winningNumber
+
+
+
+/* **** Guessing Game Functions **** */
+
+// Generate the Winning Number
+
+function generateWinningNumber(){
+	// add code here
+}
+
+// Fetch the Players Guess
+
+function playersGuessSubmission(){
+	// add code here
+}
+
+// Determine if the next guess should be a lower or higher number
+
+function lowerOrHigher(){
+	// add code here
+}
+
+// Check if the Player's Guess is the winning number 
+
+function checkGuess(){
+	// add code here
+}
+
+// Create a provide hint button that provides additional clues to the "Player"
+
+function provideHint(){
+	// add code here
+}
+
+// Allow the "Player" to Play Again
+
+function playAgain(){
+	// add code here
+}
+
+
+/* **** Event Listeners/Handlers ****  */
